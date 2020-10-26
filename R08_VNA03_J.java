30c7b9d553549fe721c2cb12cf81dbf9989ea95e
rhohman23 <54964890+rhohman23@users.noreply.github.com>
2020-10-25 22:56:36 -0400

    Create R08_VNA03_J.java

---
+++ R08_VNA03_J.java
@@ -0,0 +1,21 @@
+//VNA03-J. Do not assume that a group of calls to independently atomic methods is atomic
+//See Rule 08 - Visibility and Atomicity (VNA)
+
+final class Adder {
+  private final AtomicReference<BigInteger> first;
+  private final AtomicReference<BigInteger> second;
+ 
+  public Adder(BigInteger f, BigInteger s) {
+    first  = new AtomicReference<BigInteger>(f);
+    second = new AtomicReference<BigInteger>(s);
+  }
+ 
+  public void update(BigInteger f, BigInteger s) { // Unsafe
+    first.set(f);
+    second.set(s);
+  }
+ 
+  public BigInteger add() { // Unsafe
+    return first.get().add(second.get());
+  }
+}
