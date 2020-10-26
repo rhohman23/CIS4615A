a34feb03ab154e62e45443320c9ae6f4676cdb34
rhohman23 <54964890+rhohman23@users.noreply.github.com>
2020-10-25 22:26:16 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,5 +1,10 @@
 if (loginSuccessful) {
-logger.severe(”Userloginsucceededfor: ” + username ) ;
+logger.severe(”User login succeeded for: ” + sanitizeUser(username));
 } else {
-logger.severe(” User login failed for : ” + username ) ;
+logger.severe(” User login failed for : ” + sanitizeUser(username));
 } 
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+    ? username : "unauthorized user";
+}
