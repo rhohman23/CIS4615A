53e0ab78ef07e40c5e5c33d2e25efa3471de1547
rhohman23 <54964890+rhohman23@users.noreply.github.com>
2020-10-25 22:07:24 -0400

    Add files via upload

---
+++ CIS4615A.java
@@ -0,0 +1,9 @@
+
+public class CIS4615A {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from Riley Hohman - Fall 2020 - UCF");
+    }
+
+}
