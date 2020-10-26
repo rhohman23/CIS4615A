9b9d50fee762a0b69e70f44af7c2e3f4d8f8854c
rhohman23 <54964890+rhohman23@users.noreply.github.com>
2020-10-25 22:56:58 -0400

    Update R08_VNA03_J.java

--- R08_VNA03_J.java
+++ R08_VNA03_J.java
@@ -2,6 +2,7 @@
 //See Rule 08 - Visibility and Atomicity (VNA)
 
 final class Adder {
+  // ...
   private final AtomicReference<BigInteger> first;
   private final AtomicReference<BigInteger> second;
  
@@ -10,12 +11,14 @@ final class Adder {
     second = new AtomicReference<BigInteger>(s);
   }
  
-  public void update(BigInteger f, BigInteger s) { // Unsafe
+ 
+ 
+  public synchronized void update(BigInteger f, BigInteger s){
     first.set(f);
     second.set(s);
   }
  
-  public BigInteger add() { // Unsafe
+  public synchronized BigInteger add() {
     return first.get().add(second.get());
   }
 }
