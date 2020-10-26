583237197b9dccb65699fd050d672c9dd256d435
rhohman23 <54964890+rhohman23@users.noreply.github.com>
2020-10-25 22:53:52 -0400

    Update R07_ERR00_J.java

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -1,8 +1,15 @@
 //ERR00-J. Do not suppress or ignore checked exceptions
 //See Rule 07 - Exceptional Behavior (ERR)
 
-try {
-  //...
-} catch (IOException ioe) {
-  ioe.printStackTrace();
-}
+boolean validFlag = false;
+do {
+  try {
+    // ...
+    // If requested file does not exist, throws FileNotFoundException
+    // If requested file exists, sets validFlag to true
+    validFlag = true;
+  } catch (FileNotFoundException e) {
+    // Ask the user for a different file name
+  }
+} while (validFlag != true);
+// Use the file
